f7a3558d4ed1602cc7c5b90a78d9372ad1040086
TildaKoven <tildakoven@ucsb.edu>
2025-01-08 21:18:55 -0800

    Update prog101-using-functions.R

--- exercises/prog101-using-functions.R
+++ exercises/prog101-using-functions.R
@@ -1,13 +1,17 @@
 # Fill in the blanks so the answers are all 2. Use help for unfamiliar functions
 # if necessary.
-log10(????)
-????(4)
-ceiling(????.1)
+log10(100)
+sqrt(4)
+ceiling(1.1)
 
 # Find the functions for cosine and sine. What are the cosine and sine of 0?
-
+cos(0) <- 1
+sin(0) <- 0
 # What do you think the following functions do? Just go off the name, don't look
 # them up.
 read.csv()
+# reads the comma separated values
 boxplot()
+# creates a box plot based off the arguments
 startsWith()
+# tells you what the first letter of the argument is 
