29a83ae9cc1d0693d299bf12a0915da1e995edbb
TildaKoven <tildakoven@ucsb.edu>
2025-01-09 08:55:42 -0800

    Complete prog101 basic vectors

--- exercises/prog101-basic-vectors.R
+++ exercises/prog101-basic-vectors.R
@@ -2,10 +2,14 @@
 
 x <- c(1.0, 3.4, 9.8, 3.9, 7.3, 8.2, 3.8, 2.2, 1.9, 6.9)
 # How long are the output vectors for each of the following expressions?
-length(x) <- 1
-atan(x) <- 10
-x^2 <- 10
-max(x) <- 1
+length(x)
+# length of length(x) is 1
+atan(x)
+# length of atan(x) is 10
+x^2
+# length of x^2 is 10
+max(x)
+# length of max(x) is 1
 
 # LETTERS is a built-in vector containing the letters of the alphabet,
 # capitalized.
