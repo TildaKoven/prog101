b294634701d7a7500bd94e8db552e41dce7d62a2
TildaKoven <tildakoven@ucsb.edu>
2025-01-09 09:19:27 -0800

    Complete prog101 multiple vectors

--- exercises/prog101-multiple-vectors.R
+++ exercises/prog101-multiple-vectors.R
@@ -4,14 +4,23 @@ instrument_deployed_hm <- c(730, 915, 1345)
 instrument_deployed_h <- floor(instrument_deployed_hm) / 100
 instrument_deployed_hdec <- (instrument_deployed_hm %% 100) / 60
 instrument_deployed <- instrument_deployed_h + instrument_deployed_hdec
+# it converts the time into an hour with a decimal
 
 # Based on the code above, calculate the duration of instrument deployments
 # using the instrument recovery times below. What units do the durations have?
+# the durations are in hours with a decimal
 instrument_recovered_hm <- c(1600, 1920, 2015)
-???
-instrument_recovered <- ??? + ???
+instrument_recovered_h <- floor(instrument_recovered_hm) / 100
+instrument_recovered_hdec <- (instrument_recovered_hm %% 100) / 60
+instrument_recovered <- instrument_recovered_h + instrument_recovered_hdec
 
-instrument_durations <- ??? - ???
+instrument_durations <- instrument_recovered - instrument_deployed
 
 # Which site had the longest duration? Use conditional indexing.
 site <- c("Santa Cruz", "Santa Rosa", "San Miguel")
+max(instrument_durations)
+which.max(instrument_durations)
+site[which.max(instrument_durations)]
+longest_durations <- site[instrument_durations == max(instrument_durations)]
+site[instrument_durations == max(instrument_durations)]
+longest_durations
